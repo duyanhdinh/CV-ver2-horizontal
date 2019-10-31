716f69753577b01410d1789c9aa24c36f0f5cab1
duyanhdinh <duyanhdinh@outlook.com>
2019-10-31 14:19:22 +0700

    clear

--- src/components/common/TimeLinePart.vue
+++ src/components/common/TimeLinePart.vue
@@ -15,7 +15,47 @@
                 </div>
                 <em class="text-base sm:text-lg font-oswald font-medium my-4 uppercase">{{item.type}} / {{item.place}}</em>
                 <div class="text-base sm:text-lg font-medium my-4"></div>
-                <div class="mt-4 text-base sm:text-lg font-normal">{{item.description}}</div>
+                <div v-if="item.description" class="mt-4 text-base sm:text-lg font-normal">{{item.description}}</div>
+                <div v-if="item.project"
+                     v-for="project in item.project"
+                     :key="project.id"
+                     class="px-2"
+                >
+                    <div  class="text-base sm:text-xl font-oswald my-4"
+                          :class="[pre.text + color.c1]">
+                        <i :style="hourglass_style" class="fad fa-hand-point-right mx-2"></i>
+                        {{project.name}}
+                    </div>
+                    <div  class="text-base sm:text-lg font-oswald my-4 px-4"
+                          :class="[pre.text + color.c2]">
+                        <i :style="hourglass_style" class="fad fa-user-crown mx-2"></i>
+                        {{project.customer}}
+                    </div>
+                    <div  class="text-base sm:text-lg font-oswald my-4 px-4"
+                          :class="[pre.text + color.c2]">
+                        <i :style="hourglass_style" class="fad fa-cogs mx-2"></i>
+                        {{project.technologies}}
+                    </div>
+                    <div  class="text-base sm:text-lg font-oswald my-4 px-4"
+                          :class="[pre.text + color.c2]">
+                        <i :style="hourglass_style" class="fad fa-user-friends mx-2"></i>
+                        {{project.team_size}}
+                    </div>
+                    <div  class="text-base sm:text-lg font-oswald my-4 px-4"
+                          :class="[pre.text + color.c2]">
+                        <i :style="hourglass_style" class="fad fa-crop-alt mx-2"></i>
+                        {{project.description}}
+                    </div>
+                    <div  class=" my-4 px-8"
+                          :class="[pre.text + color.c2]">
+                        <div class="my-2" v-for="text in project.responsibility">
+                            <i :style="hourglass_style" class="fad fa-pen mx-2"></i>
+                            {{text}}
+                        </div>
+                    </div>
+
+
+                </div>
             </div>
 
         </div>
